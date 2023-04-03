75568f93c3d127d50902650bdf5c8b3660864852
Nicholas Subianto <niko.subianto@gmail.com>
2023-04-03 12:45:14 +0800

    Tried to do posts stuff, Redo training!

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -7,12 +7,19 @@ type CardsProps = {
 };
 
 const Cards = (props: CardsProps) => {
+  
+  const imageClick = (id : string) => {
+    window.location.href = "/posts/" + id;
+  }
+
   return (
     <div className="mt-4 mb-2 h-[400px] shrink-0 rounded-lg bg-teal-700 text-white shadow-lg transition-colors hover:bg-niko-orange hover:text-niko-shade">
       <img
         className="h-2/3 w-full rounded-t-lg bg-niko-shade object-contain"
         src={props.image}
         alt="Card Image"
+        //onClick={() => imageClick(props.title)}
+        onClick={() => imageClick(props.title)}
       ></img>
       <p className="m-2 font-comfortaa text-lg">{props.title}</p>
       <p className="mx-2 font-comfortaa text-sm">{props.description}</p>
---
+++ src/pages/posts/[id].js
@@ -0,0 +1,30 @@
+export async function getStaticPaths() {
+    const paths = getAllPostIds();
+    return {
+      paths,
+      fallback: false,
+    };
+  }
+    
+  import { getAllPostIds, getPostData } from '../../lib/posts';
+  
+  export async function getStaticProps({ params }) {
+    const postData = getPostData(params.id);
+    return {
+      props: {
+        postData,
+      },
+    };
+  }
+
+export default function Post({}){
+  return(
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+    </Layout>
+  );
+}
